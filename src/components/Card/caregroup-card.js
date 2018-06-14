11f992dfa8594c1631868d84ae94218c6d4063c2
Evan Lie <evan.taurusta@gmail.com>
2018-06-14 07:05:11 -0700

    added new packages

--- src/components/Card/caregroup-card.js
+++ src/components/Card/caregroup-card.js
@@ -1,41 +1,43 @@
 import React, { Component } from 'react';
+import {Alert} from 'reactstrap'
 import './caregroup-card.css';
 import Axios from 'axios';
 
 export default class caregroupCard extends Component {
-    GET_URL = 'https://fast-badlands-36660.herokuapp.com/sermons';
-    constructor() {
-        super();
-        this.state = {
-            caregroups: ["hello", "lalala", "lala"],
-            result: {}
-        }
-    }
-    componentWillMount() {
-        Axios.get(this.GET_URL) 
-        .then (res => {
-            const result = res.data;
-            this.setState({ result });
-        })
+  GET_URL = 'https://fast-badlands-36660.herokuapp.com/sermons';
+  constructor() {
+      super();
+      this.state = {
+      caregroups: ["hello", "lalala", "lala"],
+        result: {}
+      }
+  }
+  componentWillMount() {
+    Axios.get(this.GET_URL) 
+      .then (res => {
 
-    }
-    populate() {
-        let content = [];
-        for(const cg of this.state.caregroups) {
-            content.push(
-                <div> 
-                    {cg}
-                </div>
-            );
-        }
-        return content;
+      const result = res.data;
+      this.setState({ result });
+    })
+  }
+  populate() {
+      let content = [];
+      for(const cg of this.state.caregroups) {
+          content.push(
+              <div> 
+                  {cg}
+                  <Alert color="info">hello world</Alert>
+              </div>
+          );
+      }
+      return content;
     }
     render() {
-        return(
-            <div className="card-container">
-                <p>this is h1</p>
-                {this.populate()}
-            </div>
-        );
+      return(
+      <div className="card-container">
+      <p>this is h1</p>
+      {this.populate()}
+      </div>
+      );
     }
 }
