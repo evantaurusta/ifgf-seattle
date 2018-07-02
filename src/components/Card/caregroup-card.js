0d58262b9d9b1f4a2592a938a5cc0557e2a2ae8f
Evan Lie <evan.taurusta@gmail.com>
2018-07-01 19:40:21 -0700

    eslint parser update

--- src/components/Card/caregroup-card.js
+++ src/components/Card/caregroup-card.js
@@ -1,12 +1,10 @@
-import React, { Component } from "react";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
-import Typography from "@material-ui/core/Typography";
-import "./caregroup-card.css";
-import Axios from "axios";
-import Config from "./../../config.json";
+import React, { Component } from 'react';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import './caregroup-card.css';
+import Axios from 'axios';
+import Config from './../../config.json';
 
 export default class CareGroupCard extends Component {
   constructor() {
@@ -23,7 +21,7 @@ export default class CareGroupCard extends Component {
         password: process.env.REACT_APP_API_PW
       }
     };
-    Axios.get(Config.api.host + "/care-groups", config).then(res => {
+    Axios.get(Config.api.host + '/care-groups', config).then(res => {
       this.setState({
         caregroups: res.data
       });
@@ -44,13 +42,13 @@ export default class CareGroupCard extends Component {
             <img className="image" src={cg.MainPhotoPath} name={cg.Name} alt={cg.Name} />
 
             <div className="details">
-            <CardContent>
-              <Typography component="p">{cg.Name}</Typography>
-              <Typography component="p">
-                {cg.ShortName}
-                {cg.Description}
-              </Typography>
-            </CardContent>
+              <CardContent>
+                <Typography component="p">{cg.Name}</Typography>
+                <Typography component="p">
+                  {cg.ShortName}
+                  {cg.Description}
+                </Typography>
+              </CardContent>
             </div>
 
           </Card>
--- src/containers/Caregroup/Caregroup.js
+++ src/containers/Caregroup/Caregroup.js
@@ -4,15 +4,15 @@ import CareGroupCard from '../../components/Card/caregroup-card';
 import './Caregroup.css';
 
 export default class CareGroup extends Component {
-    render() {
-        return (
-            <React.Fragment>
-                <h1>Care Group</h1>
-                <div className='container'>
-                    <CareGroupCard/>
-                </div>
-                
-            </React.Fragment>
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Care Group</h1>
+        <div className='container'>
+          <CareGroupCard />
+        </div>
+
+      </React.Fragment>
+    );
+  }
 }
