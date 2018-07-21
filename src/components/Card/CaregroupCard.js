6d7d297c7ea984fa25a5914951ecf1c9aec02e5d
lkuntary <lkuntary@expedia.com>
2018-07-21 15:52:06 -0700

    address pr comments

--- src/components/Card/CaregroupCard.js
+++ src/components/Card/CaregroupCard.js
@@ -1,79 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
-import Axios from 'axios';
-import Config from './../../config.json';
 import { withStyles } from '../../../node_modules/@material-ui/core';
 
 import CardStyle from './Styles/CardStyle';
 
-class CaregroupCard extends Component {
+const CaregroupCard  = ({caregroup, classes, index}) => 
+  <React.Fragment key={index}>
+    <Card className={classes.card}>
+      <img
+        className={classes.image}
+        src={caregroup.MainPhotoPath}
+        name={caregroup.Name}
+        alt={caregroup.Name}
+      />
+      <div className={classes.details}>
+        <CardContent className={classes.content}>
+          <Typography className={classes.title} variant='title'>
+            {caregroup.Name}
+          </Typography>
+          <Typography variant='subheading'>
+            {caregroup.ShortName}
+            {caregroup.Description}
+          </Typography>
+        </CardContent>
 
-  constructor() {
-    super();
-    this.state = {
-      caregroups: [],
-      result: {}
-    };
-  }
-  componentWillMount() {
-    const config = {
-      auth: {
-        username: process.env.REACT_APP_API_USER,
-        password: process.env.REACT_APP_API_PW
-      }
-    };
-    Axios.get(Config.api.host + '/care-groups', config).then(res => {
-      this.setState({
-        caregroups: res.data
-      });
-    });
-  }
-  /**
-   * Populate card with data from the api
-   */
-  populate = () => {
-    const {classes} = this.props;
-    if (this.state.caregroups.length === 0) {
-      return null;
-    }
-    return this.state.caregroups.map((cg, index) => {
-      return (
-        <div key={index}>
-          <Card className={classes.card}>
-            <img
-              className={classes.image}
-              src={cg.MainPhotoPath}
-              name={cg.Name}
-              alt={cg.Name}
-            />
-            <div className={classes.details}>
-              <CardContent className={classes.content}>
-                <Typography className={classes.title} variant='title'>
-                  {cg.Name}
-                </Typography>
-                <Typography variant='subheading'>
-                  {cg.ShortName}
-                  {cg.Description}
-                </Typography>
-              </CardContent>
-
-            </div>
-          </Card>
-        </div>
-      );
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        {this.populate()}
       </div>
-    );
-  }
-
-}
+    </Card>
+  </React.Fragment>;
 
 export default withStyles(CardStyle)(CaregroupCard);
--- src/containers/Caregroup/Caregroup.js
+++ src/containers/Caregroup/Caregroup.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react';
+import './Caregroup.css';
+import Config from './../../config.json';
+import Axios from 'axios';
 import CaregroupCard from '../../components/Card/CaregroupCard';
 
-import './Caregroup.css';
+class CareGroup extends Component {
 
-export default class CareGroup extends Component {
+  constructor() {
+    super();
+    this.state = {
+      caregroups: [],
+      result: {}
+    };
+  }
+
+  componentDidMount() {
+    const config = {
+      auth: {
+        username: process.env.REACT_APP_API_USER,
+        password: process.env.REACT_APP_API_PW
+      }
+    };
+    Axios.get(Config.api.host + '/care-groups', config).then(res => {
+      this.setState({
+        caregroups: res.data
+      });
+    });
+  }
+
+  /**
+   * Populate card with data from the api
+   */
+  populate = () => {
+    if (this.state.caregroups.length === 0) {
+      return null;
+    }
+    return this.state.caregroups.map((cg, index) => {
+      return (
+        <CaregroupCard caregroup={cg} key={index}/> 
+      );
+    });
+  };
+
   render() {
     return (
       <React.Fragment>
         <h1>Care Group</h1>
-        <div className='caregroup'>
-          <CaregroupCard />
-        </div>
-
+        {this.populate()}
       </React.Fragment>
     );
   }
 }
+export default CareGroup;
