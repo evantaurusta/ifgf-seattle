7db09baa3f8bda40e8c44640ade9da2fd0fbd6d8
Samuel Kitono <samuel@kitono.com>
2018-07-21 15:44:04 -0700

    Refactoring design

--- src/App.js
+++ src/App.js
@@ -3,14 +3,11 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import './App.css';
 import Caregroup from './containers/Caregroup/Caregroup';
 import PrayerRequest from './containers/PrayerRequest/PrayerRequest';
+import Home from './containers/Home/Home';
+import Header from "components/Header/Header.jsx";
+import HeaderLinks from "components/Header/HeaderLinks.jsx";
 
 require('dotenv').config({ path: __dirname + '/.env' });
-
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-);
 
 const About = () => (
   <div>
@@ -18,67 +15,49 @@ const About = () => (
   </div>
 );
 
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
+const App = () => (
+    <Router>
+        <div>
+        {/* <div>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/caregroup">caregroup</Link>
+                </li>
+                <li>
+                    <Link to="/topics">Topics</Link>
+                </li>
+                <li>
+                    <Link to="/pray">Prayer Request</Link>
+                </li>
+            </ul>
+
+            <hr /> */}
+
+        <Header
+          color="white"
+          brand="Material Kit React"
+          rightLinks={<HeaderLinks />}
+          fixed
+          changeColorOnScroll={{
+            height: 400,
+            color: "white"
+          }}
+        /> 
+
+            <div id="main">
+                <Route exact path="/" component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/caregroup" component={Caregroup} />
+                <Route path="/pray" component={PrayerRequest}/>
+            </div>
+        </div>
+    </Router>
 );
 
-const Topics = ({ match }) => (
-  <div>
-    <h2>Topics</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>Components</Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.path}/:topicId`} component={Topic} />
-    <Route
-      exact
-      path={match.path}
-      render={() => <h3>Please select a topic.</h3>}
-    />
-  </div>
-);
-
-const BasicExample = () => (
-  <Router>
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/caregroup">caregroup</Link>
-        </li>
-        <li>
-          <Link to="/topics">Topics</Link>
-        </li>
-        <li>
-          <Link to="/pray">Prayer Request</Link>
-        </li>
-      </ul>
-
-      <hr />
-
-      <div id="main">
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/caregroup" component={Caregroup} />
-        <Route path="/topics" component={Topics} />
-        <Route path="/pray" component={PrayerRequest} />
-      </div>
-    </div>
-  </Router>
-);
-export default BasicExample;
+export default App;
